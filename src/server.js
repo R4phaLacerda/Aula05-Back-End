6766400a3d3e77a157e36d6041dd412bfa51c2fd
raphaelle lacerda <raphaellelacerda.tds2.senai@gmail.com>
2024-08-29 10:49:51 -0300

    feat: add some request

--- src/server.js
+++ src/server.js
@@ -8,12 +8,71 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes = [
+    {
+        Id: 1,
+        Nome: "Nojinho",
+        Cor: "Verde"
+    },
+    {
+        Id: 2,
+        Nome: "Raiva",
+        Cor: "Vermelho"
+    },
+    {
+        Id: 3,
+        Nome: "Ansiedade",
+        Cor: "Laranja"
+    },
+    {
+        Id: 4,
+        Nome: "Medo",
+        Cor: "Roxo"
+    }
+]
+
+const personagem = [
+    {
+        Id: 100,
+        Nome: "Totoro",
+        Studio: "Ghibli",
+        Vivo: true
+    },
+    {
+        Id: 101,
+        Nome: "Sal fisher",
+        Studio: "Portablemoose",
+        Vivo: false
+    },
+    {
+        Id: 102,
+        Nome: "Thiago Fritz",
+        Studio: "Rafael Lange",
+        Vivo: false
+    },
+    {
+        Id: 103,
+        Nome: "Klaus Hargreeves",
+        Studio: "Netflix",
+        Vivo: true
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
 })
 
+
 app.get("/2tds2", (req, res) => {
-    return res.status(500).send({ message: "Hello, World!" })
+    return res.status(200).send({ message: "Hello, World!" })
+})
+
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send( emocoes )
+})
+
+app.get("/personagem", (req, res) => {
+    return res.status(200).send( personagem )
 })
 
 app.listen(serverPort, () => {
